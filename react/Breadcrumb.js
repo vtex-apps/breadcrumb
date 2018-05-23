10d84f5b4a28964b426804fe4cbce851fe225f37
Felipe Sales <sales.felipeb@gmail.com>
2018-05-23 13:53:20 -0300

    Fix display of categories

--- react/Breadcrumb.js
+++ react/Breadcrumb.js
@@ -2,61 +2,87 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'render'
 
+CSS_CLASSES = {
+  LINK: 'vtex.breadcrumb__link dib pv1 link light-primary hover-primary ph2',
+  BREADCRUMB: 'vtex.breadcrumb pa4',
+}
+
 /**
  * Breadcrumb Component.
  */
-export class Breadcrumb extends Component {
-  generateCategoryLink(currentCat, list) {
+export default class Breadcrumb extends Component {
+  generateCategoriesLink(currentCat, list, i) {
     const category = {}
+    const names = currentCat.split('-').filter(el => el !== '')
 
-    const names = currentCat.split('-')
-
-    category.name = names.length > 0 ? names[names.length - 1] : names
-    category.url = list.join()
+    category.name = names.length > 0 ? names[names.length - 1] : names[0]
+    category.url = list.slice(0, i + 1).join()
 
     return (
-      <Link
-        className="vtex.breadcrumbs__link ph2"
-        page="store/search"
-        params={{ category: category.url }}
-      >
-        {category.replace('-', '')}
-      </Link>
+      <span>
+        /
+        <Link
+          className={CSS_CLASSES.LINK}
+          page="store/search"
+          params={{ term: category.url }}
+        >
+          {category.name}
+        </Link>
+      </span>
     )
+  }
+
+  treatCategories(list) {
+    let result = list || []
+
+    const regex = /^\/(.*)\//g
+
+    result = result.map(el => {
+      if (el.includes('/')) {
+        return el
+          .replace(regex, '$1')
+          .replace('/', '-')
+          .toLowerCase()
+      }
+
+      return el
+    })
+
+    if (result.length > 1) {
+      result = result.sort(function(e1, e2) {
+        if (e1.length < e2.length) {
+          return -1
+        }
+
+        if (e1.length > e2.length) {
+          return 1
+        }
+
+        return 0
+      })
+    }
+
+    return result
   }
 
   render() {
     const { search, slug, categories } = this.props
 
-    const categoriesList = categories || []
-    const categoriesUrl = categories || []
+    const categoriesList = this.treatCategories(categories)
 
     return (
-      <div className="vtex.breadcrumbs pa4">
-        <Link className="vtex.breadcrumbs__link ph2" page="store">
+      <div className={CSS_CLASSES.BREADCRUMB}>
+        <Link className={CSS_CLASSES.LINK} page="store">
           Home
         </Link>
+        {categoriesList.map((category, i) => {
+          return this.generateCategoriesLink(category, categoriesList, i)
+        })}
         /
-        {categoriesList.map(function(category) {})}
-        /
-        {search && (
-          <Link
-            className="vtex.breadcrumbs__link ph2"
-            page="store/search"
-            params={{ term: `${encodeURI(search).replace('/', '%2F')}` }}
-          >
-            {search.replace('/', ' ')}
-          </Link>
-        )}
-        {slug && (
-          <Link
-            className="vtex.breadcrumbs__link ph2"
-            page="store/product"
-            params={{ slug: `${encodeURI(slug)}` }}
-          >
-            {slug}
-          </Link>
-        )}
+        <span className="ph2">
+          {categories ? '' : search}
+          {slug}
+        </span>
       </div>
     )
   }
@@ -64,11 +90,9 @@ export class Breadcrumb extends Component {
 
 Breadcrumb.propTypes = {
   /** Search term used **/
-  search: PropTypes.string.isRequired,
+  search: PropTypes.string,
   /** Product's slug**/
   slug: PropTypes.string,
   /** Categories*/
   categories: PropTypes.arrayOf(PropTypes.string),
 }
-
-export default Breadcrumb

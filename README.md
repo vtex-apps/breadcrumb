3b38bb3722c9fd0bea7f4f4d74260b6fdc61b1d7
Felipe Sales <sales.felipeb@gmail.com>
2018-05-24 11:43:08 -0300

    Add the Breadcrumbs component (#1)
    * inint
    
    * Fix display of categories
    
    * Fix css order
    
    * add github files
    
    * Update component docs to the new template
    
    * Fix readme link
    
    * Fix readme and gitignore
    
    * Fix README, change requested on PR

---
+++ README.md
@@ -0,0 +1,63 @@
+# Breadcrumb
+Breadcrumb is a canonical component that any VTEX app can use.
+
+To use it in your code you should first add it as a dependencie into your app's ```manifest.json```, like:
+
+```json
+"dependencies": {
+    "vtex.breadcrumb": "0.x",
+}
+```
+
+## Usage
+You can use it in your code through an extension point in your app.
+
+```javascript
+    <ExtensionPoint
+        id="breadcrumb"
+        search={query}
+        categories={categories}
+    />
+```
+
+And then link the component in the extensions section in the pages.json file.
+
+```javascript
+{
+ "pages": {
+    "store/product": {
+      "path": "/:slug/p"
+    }
+  },
+  "extensions": {
+    "store/product/breadcrumb": {
+      "component": "vtex.breadcrumb/Breadcrumb"
+    }
+  }
+}
+```
+
+| Prop name          | Type           | Description                                                                 |
+| ------------------ | -------------- | --------------------------------------------------------------------------- |
+| `search`           | `String`       | Term used to get to that specific page on the website                       |
+| `slug`             | `String`       | Product's unique identification stored on the database                      |
+| `categories`       | `Array(String)`| List of categories which the product belongs to                             |
+
+## Extra info:
+
+The product's categories should appear as an array in one of this two formats:
+
+- 1  
+
+```javascript
+categories = ['/Eletronics/','/Eletronics/Computers']
+```
+
+- 2
+
+```javascript
+categories = ['eletronics','eletronics-computers']
+```
+
+
+See an example at [Dreamstore](https://github.com/vtex-apps/dreamstore-theme/blob/master/react/components/GalleryWrapper.js) app
---
+++ react/Breadcrumb.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'render'
+
+const CSS_CLASSES = {
+  BREADCRUMB: 'vtex-breadcrumb pa4',
+  LINK: 'vtex-breadcrumb__link dib pv1 link light-primary hover-primary ph2',
+}
+
+/**
+ * Breadcrumb Component.
+ */
+export default class Breadcrumb extends Component {
+  generateCategoriesLink(currentCat, list, i) {
+    const category = {}
+    const names = currentCat.split('-').filter(el => el !== '')
+
+    category.name = names.length > 0 ? names[names.length - 1] : names[0]
+    category.url = list.slice(0, i + 1).join()
+
+    return (
+      <span>
+        /
+        <Link
+          className={CSS_CLASSES.LINK}
+          page="store/search"
+          params={{ term: category.url }}
+        >
+          {category.name}
+        </Link>
+      </span>
+    )
+  }
+
+  treatCategories(list) {
+    let result = list || []
+
+    const regex = /^\/(.*)\//g
+
+    result = result.map(el => {
+      if (el.includes('/')) {
+        return el
+          .replace(regex, '$1')
+          .replace('/', '-')
+          .toLowerCase()
+      }
+
+      return el
+    })
+
+    if (result.length > 1) {
+      result = result.sort(function(e1, e2) {
+        if (e1.length < e2.length) {
+          return -1
+        }
+
+        if (e1.length > e2.length) {
+          return 1
+        }
+
+        return 0
+      })
+    }
+
+    return result
+  }
+
+  render() {
+    const { search, slug, categories } = this.props
+
+    const categoriesList = this.treatCategories(categories)
+
+    return (
+      <div className={CSS_CLASSES.BREADCRUMB}>
+        <Link className={CSS_CLASSES.LINK} page="store">
+          Home
+        </Link>
+        {categoriesList.map((category, i) => {
+          return this.generateCategoriesLink(category, categoriesList, i)
+        })}
+        /
+        <span className="ph2">
+          {categories ? '' : search}
+          {slug}
+        </span>
+      </div>
+    )
+  }
+}
+
+Breadcrumb.propTypes = {
+  /** Search term used **/
+  search: PropTypes.string,
+  /** Product's slug**/
+  slug: PropTypes.string,
+  /** Categories*/
+  categories: PropTypes.arrayOf(PropTypes.string),
+}

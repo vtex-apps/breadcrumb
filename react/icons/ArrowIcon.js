49e0206bea1cea173043d892df0b1c10eb47e086
Felipe Sales <sales.felipeb@gmail.com>
2018-08-07 09:42:03 -0300

    Use arrow Icon insteadof simple: >

---
+++ react/icons/ArrowIcon.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+/**
+ * Arrow Icon component in svg
+ */
+export default class ArrowIcon extends Component {
+  render() {
+    return (
+      <svg
+        width="5"
+        height="8"
+        viewBox="0 0 5 8"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M4.70449 4.30069C4.31457 4.68774 3.68543 4.68774 3.29551 4.30069L0 1.02941L1.03704 0L4 2.94118L6.96296 0L8 1.02941L4.70449 4.30069Z"
+          transform="translate(0 8) rotate(-90)"
+          fill="#979899"
+        />
+      </svg>
+    )
+  }
+}

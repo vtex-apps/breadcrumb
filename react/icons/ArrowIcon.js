c429b0251f358b31c7d5eb2ecac0624c8224906a
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-11-01 10:57:09 -0300

    Move to function component and add xlink:href attribute as fallback

--- react/icons/ArrowIcon.js
+++ react/icons/ArrowIcon.js
@@ -1,20 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 /**
  * Arrow Icon component in svg
  */
-export default class ArrowIcon extends Component {
-  render() {
-    return (
-      <svg
-        width="5"
-        height="8"
-        viewBox="0 0 5 8"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <use href="#arrow-right" />
-      </svg>
-    )
-  }
+const ArrowIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      version="1.1"
+      width="5"
+      height="8"
+      viewBox="0 0 5 8"
+      fill="none"
+    >
+      <use href="#arrow-right" xlinkHref="#arrow-right" />
+    </svg>
+  )
 }
+
+export default ArrowIcon
--- react/icons/HomeIcon.js
+++ react/icons/HomeIcon.js
@@ -1,37 +1,37 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 /**
  * Home Icon component in svg
  */
-export default class HomeIcon extends Component {
-  static propTypes = {
-    /* Percentage size of the icon */
-    size: PropTypes.number,
-    /* Fill color for the icon */
-    fillColor: PropTypes.string,
-  }
-
-  static defaultProps = {
-    size: 25,
-    fillColor: '#979899',
-  }
-
-  render() {
-    const { size, fillColor } = this.props
-
-    return (
-      <svg
-        width={size}
-        height={size}
-        viewBox="0 0 576 512"
-        xmlns="http://www.w3.org/2000/svg"
-        className="vtex-icon__home-icon relative"
-        style={{ top: '5px' }}
-        color={fillColor}
-      >
-        <use href="#home" />
-      </svg>
-    )
-  }
+const HomeIcon = ({ size, fillColor }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      className="vtex-icon__home-icon relative"
+      viewBox="0 0 576 512"
+      version="1.1"
+      width={size}
+      height={size}
+      style={{ top: '5px' }}
+      color={fillColor}
+    >
+      <use href="#home" xlinkHref="#home" />
+    </svg>
+  )
 }
+
+HomeIcon.propTypes = {
+  /* Percentage size of the icon */
+  size: PropTypes.number,
+  /* Fill color for the icon */
+  fillColor: PropTypes.string,
+}
+
+HomeIcon.defaultProps = {
+  size: 25,
+  fillColor: '#979899',
+}
+
+export default HomeIcon

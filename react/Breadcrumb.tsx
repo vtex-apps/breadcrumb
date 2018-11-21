4e3027e6bdfff63073eb8409f133396f244a3ce1
Theo Moura <theomouram@gmail.com>
2018-11-21 16:50:32 -0300

    Remove accentuation on links and convert to lowercase

--- react/Breadcrumb.tsx
+++ react/Breadcrumb.tsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types'
 import React, { Component, Fragment, ReactNode } from 'react'
 import { Link } from 'render'
+import unorm from 'unorm'
 
 import ArrowIcon from './icons/ArrowIcon'
 import HomeIcon from './icons/HomeIcon'
@@ -45,7 +46,8 @@ class Breadcrumb extends Component<Props> {
       let categoryStripped = category.replace(/^\//, '').replace(/\/$/, '')
       const categories = categoryStripped.split('/')
       const [categoryKey] = categories.reverse()
-      categoryStripped = categories.length === 1 ? `${categoryStripped.toLowerCase()}/d` : categoryStripped
+      categoryStripped = unorm.nfd(categoryStripped).toLowerCase().replace(/[\u0300-\u036f]/g, '')
+      categoryStripped += categories.length === 1 ? '/d' : ''
       return {
         name: categoryKey.toLowerCase(),
         value: categoryStripped,

7882c301f1e82ac124f6cacc4328dcfad5886947
Augusto Lazaro <aemlazaro@gmail.com>
2019-04-24 17:04:41 -0300

    Create product-breadcrumb block to apply product context

---
+++ react/BreadcrumbProduct.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react'
+import { ProductContext } from 'vtex.product-context'
+import { path, isEmpty } from 'ramda'
+
+import Breadcrumb from './Breadcrumb';
+
+interface Props {
+  term?: string | undefined
+  categories: Array<string> | undefined
+}
+
+const BreadcrumbProduct = (props: Props) => {
+  const valuesFromContext = React.useContext(ProductContext)
+
+  const defProps: Props = !valuesFromContext || isEmpty(valuesFromContext)
+    ? props
+    : { term: path(['product', 'productName'], valuesFromContext), categories: path(['categories'], valuesFromContext) }
+
+  const { term, categories } = defProps
+
+  return (
+    <Breadcrumb 
+      term={term} 
+      categories={categories} 
+    />
+  )
+}
+
+BreadcrumbProduct.defaultProps = {
+  categories: [],
+}
+
+export default BreadcrumbProduct

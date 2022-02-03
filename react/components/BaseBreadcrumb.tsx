106a47d04cbcaba8d6b9fa7cbd93475f6356f030
Avanash  Mungan <88778617+Avanashm@users.noreply.github.com>
2022-02-03 16:04:36 +0200

    home option upadate

--- react/components/BaseBreadcrumb.tsx
+++ react/components/BaseBreadcrumb.tsx
@@ -83,12 +83,12 @@ const Breadcrumb: React.FC<Props> = ({
   return (
     <div data-testid="breadcrumb" className={`${handles.container} pv3`}>
      {/* The "home page" option on the breadcrumbs  */}
-      {/* <Link
+      <Link
         className={`${handles.link} ${handles.homeLink} ${linkBaseClasses} v-mid`}
         page="store.home"
       >
        Home
-      </Link> */}
+      </Link>
       {navigationList.map(({ name, href }, i) => {
         let decodedName = ''
 
